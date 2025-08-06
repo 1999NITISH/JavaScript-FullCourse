bb9ad485d6fb83b90978c953498d6424bf02a0dd
Nitish Kumar <91321018+1999NITISH@users.noreply.github.com>
2025-08-06 07:09:47 +0000

    data part is done

---
+++ 1-basic/datatypes.js
@@ -0,0 +1,39 @@
+//Primitive Data Types:
+//These represent single, simple data values and are immutable (cannot be changed after creation).
+//Number 
+let age = 24;
+let price = 34.45;
+
+console.log([age, price]);
+//String
+let name = "Nitish Kumar";
+let greeting = "Hello, Nitish!";
+console.log([name, greeting]);
+//boolean
+let isActive = true;
+let isLoggedInt = false;
+console.log([isActive, isLoggedInt]);
+//null
+let emptyValue = null;
+console.log(emptyValue0);
+//undefined
+//let notAssigned;
+//console.log(notAssigned);
+/*JavaScript has 8 Datatypes
+String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+Object
+
+The Object Datatype
+The object data type can contain both built-in objects, and user defined objects:
+
+Built-in object types can be:
+
+objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more.*/
+const cars = ["bmw", "tata", "Mahindra",];
+console.log(cars);
